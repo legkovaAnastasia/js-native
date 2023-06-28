7824c15719c3c41474e8443deac4c49eb816e0ab
anastasia <legkova.anastasia127@gmail.com>
2023-06-28 15:34:04 +0300

    first commit

---
+++ src/01-hello-tests/01.test.ts
@@ -0,0 +1,11 @@
+
+export function splitSmth(sentence:string)  {
+    return ['hello', 'my', 'friends']
+}
+
+export function sum(a:number, b:number) {
+    return a + b
+}
+function mult(a:number, b:number) {
+    return a * b
+}
